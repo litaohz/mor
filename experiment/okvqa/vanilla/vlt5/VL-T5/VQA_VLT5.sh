4eee76a8c722b0ef5f260c9c38dbfbd25471f124
Tao Li <taoli1@microsoft.com>
2024-06-08 12:25:58 +0800

    update

---
+++ experiment/okvqa/vanilla/vlt5/VL-T5/VQA_VLT5.sh
@@ -0,0 +1,26 @@
+# The name of experiment
+name=VLT5
+
+output=snap/vqa/$name
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
+
+PYTHONPATH=$PYTHONPATH:./src \
+python -m torch.distributed.launch \
+    --nproc_per_node=$1 \
+    src/vqa.py \
+        --distributed --multiGPU \
+        --train karpathy_train \
+        --valid karpathy_val \
+        --test OpenEnded_mscoco_val2014_questions_new_labelled \
+        --optim adamw \
+        --warmup_ratio 0.1 \
+        --clip_grad_norm 5 \
+        --lr 5e-5 \
+        --epochs 20 \
+        --num_workers 4 \
+        --backbone 't5-base' \
+        --output $output ${@:2} \
+        --load snap/pretrain/VLT5/Epoch30 \
+        --num_beams 5 \
+        --batch_size 80 \
+        --valid_batch_size 100 \
